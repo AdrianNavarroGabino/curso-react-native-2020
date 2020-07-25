5028497a8487cf00d291a8a91c0cfa9e10563c74
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-07-25 09:30:09 +0200

    Crear, importar y usar componentes

--- ReactNativeTodo/App.js
+++ ReactNativeTodo/App.js
@@ -1,17 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Main from "./src/screens/main";
 
 // En el emulador de Android
 // ctrl + M -> Opciones (reload, debug...)
 // R + R -> Reload
 
 export default function App() {
-  return (
+  /*return (
     <View style={styles.container}>
       <Text>Hola mundo!</Text>
     </View>
-  );
+  );*/
+
+  return <Main />;
 }
 
 const styles = StyleSheet.create({
