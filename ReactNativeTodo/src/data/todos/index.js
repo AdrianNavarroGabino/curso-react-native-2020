d8898a219bc4abc484564bd5973cee925a7003e0
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-07-25 10:25:40 +0200

    State y setState

---
+++ ReactNativeTodo/src/data/todos/index.js
@@ -0,0 +1,7 @@
+const getTodos = () => [
+    {text: "Tarea 1", done: false},
+    {text: "Tarea 2", done: false},
+    {text: "Tarea 3", done: false},
+    {text: "Tarea 4", done: true}];
+
+export {getTodos};
--- ReactNativeTodo/src/screens/main/index.js
+++ ReactNativeTodo/src/screens/main/index.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import {View, Text, StyleSheet} from "react-native";
 // He definido "name": "todoList" en package.json para definir el nombre de la carpeta raíz
 import TodoList from "todoList/src/components/TodoList";
+import {getTodos} from "todoList/src/data/todos";
 
 const styles = StyleSheet.create({
     container: {
@@ -12,20 +13,26 @@ const styles = StyleSheet.create({
     },
 });
 
-//const todos = ["Tarea 1", "Tarea 2", "Tarea 3"];
-const todos = [
-    {text: "Tarea 1", done: false},
-    {text: "Tarea 2", done: false},
-    {text: "Tarea 3", done: false},
-    {text: "Tarea 4", done: true}];
-
 class MainScreen extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todos: []
+        };
+    }
+
+    componentDidMount = () => {
+        this.setState({todos:getTodos()});
+    }
+
     render() {
+        //const {todos} = this.state;
         return (
             <View style={styles.container}>
                 <Text selectable>ToDo List App</Text>
 
-                <TodoList todos={todos}/>
+                <TodoList todos={this.state.todos}/>
             </View>
         );
     }
