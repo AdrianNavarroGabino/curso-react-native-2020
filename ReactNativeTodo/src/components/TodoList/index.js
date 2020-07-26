df9acc01838a1e0e0d6313d3cb44145ec408c825
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-07-26 16:07:43 +0200

    FlatList

--- ReactNativeTodo/src/components/TodoList/index.js
+++ ReactNativeTodo/src/components/TodoList/index.js
@@ -1,21 +1,23 @@
 import React, {Fragment} from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, ScrollView, FlatList } from 'react-native';
 // rncsl + tab
 
 const styles = StyleSheet.create({
     container: {
         width: '100%'
     },
+    contentContainer: {
+        flexGrow: 1
+    },
     listItem: {
-        borderWidth: 1,
         margin: 5,
-        width: "80%",
+        padding: 5,
+        width: "100%",
         flexDirection: 'row',
         alignItems: 'center'
     },
     bullet: {
-        padding: 5,
-        fontWeight: 'bold'
+        width: '10%'
     },
     text: {
         flex: 1,
@@ -36,28 +38,67 @@ const styles = StyleSheet.create({
     deleteText: {
         color: '#ff0000',
         fontSize: 18
+    },
+    emptyList: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    separator: {
+        height: 1,
+        width: "86%",
+        backgroundColor: "#CED0CE",
+        marginLeft: "14%"
     }
 })
 
 const TodoList = ({
     todos, onUpdate, onDelete
-}) => (
+}) => {
+    renderItem = todo => (
+        <TouchableOpacity style={styles.listItem} key={todo.text} onPress={() => onUpdate({...todo, done: !todo.done})}>
+            <Text style={styles.bullet}>-</Text>
+            <Text style={[styles.text, todo.done && styles.textDone]}>{todo.text}</Text>
+            <TouchableOpacity style={styles.delete} onPress={() => onDelete(todo)}>
+                <Text style={styles.deleteText}>X</Text>
+            </TouchableOpacity>
+        </TouchableOpacity>
+    )
+    
+    renderSeparator = () => {
+        return <View style={styles.separator} />;
+    };
+    
+    renderEmptyComponent = () => (
+        <View style={styles.emptyList}>
+          <Text>Lista Vacia</Text>
+        </View>
+    );
+    
+    return (
+        <FlatList
+            style={styles.container}
+            data={todos}
+            keyExtractor={todo => todo.id}
+            renderItem={({item}) => renderItem(item)}
+            ItemSeparatorComponent={renderSeparator}
+            ListEmptyComponent={renderEmptyComponent}
+        />)
+    /*
     // Un ScrollView va a renderizar todos los elementos de la lista
     // Por eso no se deben utilizar para listas, sino para elementos en pantalla
-    <ScrollView contentContainerStyle={styles.container}>
-        {/*<Text>Tarea 1</Text>
-        <Text>Tarea <Text>2</Text></Text>
-        <Text>Tarea 3</Text>*/}
+    //<ScrollView contentContainerStyle={styles.container}>
+        //<Text>Tarea 1</Text>
+        //<Text>Tarea <Text>2</Text></Text>
+        //<Text>Tarea 3</Text>
 
-        {/*<Text>{todos[0]}</Text>
-        <Text>{todos[1]}</Text>
-        <Text>{todos[2]}</Text>*/}
+        //<Text>{todos[0]}</Text>
+        //<Text>{todos[1]}</Text>
+        //<Text>{todos[2]}</Text>
 
         {todos.map(todo => (
-            /*
             Equivalente a
             todo.done ? null : <Text key={todo.text}>{todo.text}</Text>
-            */
             <TouchableOpacity style={styles.listItem} key={todo.text} onPress={() => onUpdate({...todo, done: !todo.done})}>
                 <Text style={styles.bullet}>-</Text>
                 <Text style={[styles.text, todo.done && styles.textDone]}>{todo.text}</Text>
@@ -67,6 +108,7 @@ const TodoList = ({
             </TouchableOpacity>
         ))}
     </ScrollView>
-);
+    */
+};
 
 export default TodoList;
